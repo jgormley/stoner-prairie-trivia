c5df86644ee5f6b43dabe35b3353ca3705ea02e8
Gormley <gormleyj@amazon.com>
2018-02-19 14:20:40 -0600

    updated questions

--- lambda/custom/question.js
+++ lambda/custom/question.js
@@ -2,65 +2,169 @@
 
 module.exports = {
     /**
-     * When editing your questions pay attention to your punctuation. Make sure you use question marks.
+     * When editing the questions pay attention to punctuation. Make sure you use question marks.
      * Make sure the first answer is the correct one. Set at least ANSWER_COUNT answers, any extras will be shuffled in.
      */
     QUESTIONS_EN_US: [
         {
             'Where is Stoner Prairie Elementary School located?': [
-                'Fitchburg',
-                'Madison',
-                'Verona',
-                'Oregon',
+                'Stoner Prairie is located in Fitchburg',
+                'Stoner Prairie is located in Madison',
+                'Stoner Prairie is located in Verona',
+                'Stoner Prairie is located in Oregon',
             ],
         },
         {
             'How many languages is the school website available in? ': [
-                '11',
-                '3',
-                '5',
-                '7',
-                '9',
+                'The Stoner Prairie website is available in 11 languages',
+                'The Stoner Prairie website is available in 3 languages',
+                'The Stoner Prairie website is available in 5 languages',
+                'The Stoner Prairie website is available in 7 languages',
+                'The Stoner Prairie website is available in 9 languages',
             ],
         },
         {
             'How many students are at Stoner Prairie?': [
-                '400',
-                '200',
-                '300',
-                '100',
+                'There are 360 students at Stoner Prairie',
+                'There are 2500 students at Stoner Prairie',
+                'There are 3000 students at Stoner Prairie',
+                'There are 110 students at Stoner Prairie',
             ],
         },
         {
             'How many classrooms are in Stoner Prairie?': [
-                '24',
-                '12',
-                '48',
-                '72',
+                'There are 24 classrooms at Stoner Prairie',
+                'There are 12 classrooms at Stoner Prairie',
+                'There are 48 classrooms at Stoner Prairie',
+                'There are 72 classrooms at Stoner Prairie',
             ],
         },
         {
             'How many students participate in Safety Patrol?': [
-                '50',
-                '200',
-                '12',
-                '400',
+                '61 students participate in Safety Patrol',
+                '200 students participate in Safety Patrol',
+                '12 students participate in Safety Patrol',
+                '400 students participate in Safety Patrol',
             ],
         },
         {
             'How many books are in the school library?': [
-                '5000',
-                '5',
-                '50',
-                '500',
+                'There are 5000 books in the library',
+                'There are 5 books in the library',
+                'There are 50 books in the library',
+                'There are 500 books in the library',
             ],
         },
         {
             'In what year was Stoner Prairie built?': [
-                '1987',
-                '1979',
-                '2000',
-                '2010',
+                'Stoner Prairie was built in 1987',
+                'Stoner Prairie was built in 1979',
+                'Stoner Prairie was built in 2000',
+                'Stoner Prairie was built in 2010',
+            ],
+        },
+        {
+            'How many teachers are there in Stoner Prairie?': [
+                '',
+                '',
+                '',
+                '',
+            ],
+        },
+        {
+            'Who is the PE teacher at Stoner Prairie?': [
+                'The P E teacher is Ms Tronson',
+                'The P E teacher is Mr Bean',
+                'The P E teacher is Ms Christianson',
+                'The P E teacher is George Washington',
+            ],
+        },
+        {
+            'How many types of hot lunch are served at Stoner Prairie?': [
+                '',
+                '',
+                '',
+                '',
+            ],
+        },
+        {
+            'How many basketball hoops are there in the gyms at Stoner Prairie?': [
+                'There are 8 basketball hoops in the Stoner Prairie gyms',
+                'There are 6 basketball hoops in the Stoner Prairie gyms',
+                'There are 12 basketball hoops in the Stoner Prairie gyms',
+                'There are 50 basketball hoops in the Stoner Prairie gyms',
+            ],
+        },
+        {
+            'How many drinking fountains are there in Stoner Prairie?': [
+                '',
+                '',
+                '',
+                '',
+            ],
+        },
+        {
+            'How many 4th and 5th grade teachers are there at Stoner Prairie?': [
+                'there are 6 4th and 5th grade teachers',
+                'there are 60 4th and 5th grade teachers',
+                'there are 600 4th and 5th grade teachers',
+                'there are 2 4th and 5th grade teachers',
+            ],
+        },
+        {
+            'How many class pets are there in Stoner Prairie?': [
+                '',
+                '',
+                '',
+                '',
+            ],
+        },
+        {
+            'How many entrances are there to get into Stoner Prairie?': [
+                '',
+                '',
+                '',
+                '',
+            ],
+        },
+        {
+            'How many subjects are taught at Stoner Prairie?': [
+                '',
+                '',
+                '',
+                '',
+            ],
+        },
+        {
+            'Which of the following subjects is not taught at Stoner Prairie?': [
+                'Sorcery is not taught at Stoner Prairie',
+                'Art is not taught at Stoner Prairie',
+                'Math is not taught at Stoner Prairie',
+                'Reading is not taught at Stoner Prairie',
+            ],
+        },
+        {
+            'How many buses are used for the students at Stoner Prairie?': [
+                '',
+                '',
+                '',
+                '',
+            ],
+        },
+        {
+            'How many teachers help with recess every day?': [
+                '',
+                '',
+                '',
+                '',
+            ],
+        },
+        {
+            'What year were iPads given to all of the students at Stoner Prairie?': [
+                '2017 was the first year students received iPads',
+                '2013 was the first year students received iPads',
+                '2010 was the first year students received iPads',
+                '1999 was the first year students received iPads',
             ],
         },
     ],
